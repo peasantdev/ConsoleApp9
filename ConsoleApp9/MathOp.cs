1db8d345da7308ae3d25bb3d64bd09648694108b
peasantdev <peasantwebdev@outlook.com>
2023-05-03 17:42:21 -0700

    Add project files.

---
+++ ConsoleApp9/MathOp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+     class MathOp
+    {
+        public int Math(int num1, int num2 = 1) 
+        {
+            int result = num1 * num2;
+            return result;
+        } 
+        
+    }
+}
+ 
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOp mathinst = new MathOp();//Instantiate the MathOp class
+            Console.WriteLine("Enter an interger: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second integer, or just press enter: ");
+            try
+            {
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int results = mathinst.Math(num1, num2);
+                Console.WriteLine(num1 + " X " + num2 + " = " + results);
+            }
+            catch
+            {
+                int results = mathinst.Math(num1);
+                Console.WriteLine(num1 + " x Default 1 = " + results);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
